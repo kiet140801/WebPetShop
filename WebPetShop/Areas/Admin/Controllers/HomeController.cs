c607af0282104371a2178db73caab8b1236be130
Dell PC <Dell PC@DESKTOP-FM9EFRD>
2024-06-17 22:41:00 +0700

    Add project files.

---
+++ WebPetShop/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestTemplate.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("admin")]
+    public class HomeController : Controller
+    {
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebPetShop/Models/Entities/PetShopDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebPetShop.Models.Entities
+{
+    public class PetShopDbContext : DbContext
+    {
+        public PetShopDbContext() { }
+        public PetShopDbContext(DbContextOptions<PetShopDbContext> options) : base(options) { }
+
+    }
+}
